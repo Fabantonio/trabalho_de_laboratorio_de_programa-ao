982ee8c594bab45451f806bb00e326dc26c09181
Fabantonio <86171123+Fabantonio@users.noreply.github.com>
2021-08-19 19:30:07 -0300

    Update trabalho CMA.c

--- trabalho CMA.c
+++ trabalho CMA.c
@@ -3,9 +3,8 @@
 #include <string.h>
 #define MAX 100
 
-int main()
+typedef struct
 {
-
     char tipo_de_peca[MAX];
     char marca[MAX];
     char cor[MAX];
@@ -16,6 +15,119 @@ int main()
     char tipo_de_logo[MAX];
     char composicao_do_tecido[MAX];
     int garantia_do_fornecedor;
+} Produto;
+
+Produto pesquisaProduto()
+{
+    Produto prod;
+    printf("Digite o tipo de peca: ");
+    gets(prod.tipo_de_peca);
+
+    if (strcmp(prod.tipo_de_peca, "Blusa") == 0 || strcmp(prod.tipo_de_peca, "blusa") == 0)
+    {
+        prod.valor = 120.00;
+    }else if (strcmp(prod.tipo_de_peca, "Calca") == 0 || strcmp(prod.tipo_de_peca, "calca") == 0)
+    {
+        prod.valor = 90.00;
+    }else if (strcmp(prod.tipo_de_peca,"Camisa")==0||strcmp(prod.tipo_de_peca,"camisa")==0)
+    {
+        prod.valor = 80.00;
+    }else if (strcmp(prod.tipo_de_peca, "Camiseta") == 0 || strcmp(prod.tipo_de_peca, "camiseta") == 0)
+    {
+        prod.valor = 40.00;
+    }else if (strcmp(prod.tipo_de_peca, "Tenis") == 0 || strcmp(prod.tipo_de_peca, "tenis") == 0)
+    {
+        prod.valor = 180.00;
+    }
+
+    printf("Digite a marca: ");
+    gets(prod.marca);
+
+    printf("Digite a cor: ");
+    gets(prod.cor);
+
+    printf("Digite o genero: ");
+    gets(prod.genero);
+
+    printf("Digite o tipo de logo: ");
+    gets(prod.tipo_de_logo);
+    if (strcmp(prod.tipo_de_logo, "Bordada") == 0 || strcmp(prod.tipo_de_logo, "bordada") == 0)
+    {
+        prod.valor += 10.00;
+    }
+
+    printf("Digite a composicao: ");
+    gets(prod.composicao_do_tecido);
+    if (strcmp(prod.composicao_do_tecido, "Dry Fit") == 0 || strcmp(prod.composicao_do_tecido, "dry fit") == 0)
+    {
+        prod.valor += 10.00;
+    }
+
+    printf("Digite o tamanho: ");
+    scanf("%d", &prod.tamanho);
+
+    while (prod.tamanho < 0)
+    {
+        printf("Valor invalido, digite novamente: ");
+        scanf("%d", &prod.tamanho);
+    }
+
+    //Numero da peça
+    printf("Digite o numero da peca: ");
+    scanf("%d", &prod.numero_da_peca);
+
+    while (prod.numero_da_peca < 0)
+    {
+        printf("Valor invalido, digite novamente: ");
+        scanf("%d", &prod.numero_da_peca);
+    }
+
+    //Garantia do fornecedor
+    prod.garantia_do_fornecedor = 2;
+
+    return prod;
+}
+
+void imprimeCompra(Produto prod)
+{
+    printf("\n-----Detalhes da compra-----");
+    printf("\n");
+    printf("\n");
+    printf("Tipo de peca: ");
+    puts(prod.tipo_de_peca);
+    printf("\n");
+    printf("Marca: ");
+    puts(prod.marca);
+    printf("\n");
+    printf("Cor: ");
+    puts(prod.cor);
+    printf("\n");
+    printf("Genero: ");
+    puts(prod.genero);
+    printf("\n");
+    printf("Tamanho: %d", prod.tamanho);
+    printf("\n");
+    printf("\n");
+    printf("Valor: %.2f", prod.valor);
+    printf("\n");
+    printf("\n");
+    printf("Numero da peca: %d", prod.numero_da_peca);
+    printf("\n");
+    printf("\n");
+    printf("Tipo de logo: ");
+    puts(prod.tipo_de_logo);
+    printf("\n");
+    printf("Composicao do tecido: ");
+    puts(prod.composicao_do_tecido);
+    printf("\n");
+    printf("Garantia do fornecedor: %d meses", prod.garantia_do_fornecedor);
+    printf("\n");
+    printf("\n");
+}
+
+int main()
+{
+
     int resp;
     char opcao, aux;
     int encerrar;
@@ -24,266 +136,12 @@ int main()
     {
         do
         {
-            //Escolha do tipo de peça
-            printf("-Escolha o tipo de peca desejado-\n\n");
-            printf("A - Blusa - RS120.00\nB - Calca - RS90.00\nC - Camisa - RS80.00\nD - Camiseta - RS65.00\nE - Short - RS40.00\nF - Tenis - RS180.00\n\n");
-            printf("Opcao: ");
-            scanf("%c", &opcao);
-            scanf("%c", &aux);
-
-            switch (opcao)
-            {
-            case 'a':
-            case 'A':
-                strcpy(tipo_de_peca, "Blusa");
-                valor = 120.00;
-                break;
-            case 'b':
-            case 'B':
-                strcpy(tipo_de_peca, "Calca");
-                valor = 90.00;
-                break;
-            case 'c':
-            case 'C':
-                strcpy(tipo_de_peca, "Camisa");
-                valor = 80.00;
-                break;
-            case 'd':
-            case 'D':
-                strcpy(tipo_de_peca, "Camiseta");
-                valor = 65.00;
-                break;
-            case 'e':
-            case 'E':
-                strcpy(tipo_de_peca, "Shorts");
-                valor = 40.00;
-                break;
-            case 'f':
-            case 'F':
-                strcpy(tipo_de_peca, "Tenis");
-                valor = 180.00;
-                break;
-            default:
-                printf("Opcao Invalida!!!\n");
-                break;
-            }
-            //printf("Tipo de peca: ");
-            //puts(tipo_de_peca);
-
-            //Escolha da marca
-
-            printf("-Escolha a marca-\n\n");
-            printf("A - Nike\nB - Puma\nC - Adidas\n\n");
-            printf("Opcao: ");
-            scanf("%c", &opcao);
-            scanf("%c", &aux);
-
-            switch (opcao)
-            {
-            case 'a':
-            case 'A':
-                strcpy(marca, "Nike");
-                break;
-            case 'b':
-            case 'B':
-                strcpy(marca, "Puma");
-                break;
-            case 'c':
-            case 'C':
-                strcpy(marca, "Adidas");
-                break;
-            default:
-                printf("Opcao Invalida!!!\n");
-                break;
-            }
-
-            //Escolha da cor
-
-            printf("-Escolha a cor-\n\n");
-            printf("A - Azul\nB - Preto\nC - Branco\nD - Laranja\nE - Amarelo\nF - Verde\nG - Rosa\n\n");
-            printf("Opcao: ");
-            scanf("%c", &opcao);
-            scanf("%c", &aux);
-
-            switch (opcao)
-            {
-            case 'a':
-            case 'A':
-                strcpy(cor, "Azul");
-                break;
-            case 'b':
-            case 'B':
-                strcpy(cor, "Preto");
-                break;
-            case 'c':
-            case 'C':
-                strcpy(cor, "Branco");
-                break;
-            case 'd':
-            case 'D':
-                strcpy(cor, "Laranja");
-                break;
-            case 'e':
-            case 'E':
-                strcpy(cor, "Amarelo");
-                break;
-            case 'f':
-            case 'F':
-                strcpy(cor, "Verde");
-                break;
-            case 'g':
-            case 'G':
-                strcpy(cor, "Rosa");
-            break;
-            default:
-                printf("Opcao Invalida!!!\n");
-                break;
-            }
-
-            //Escolha do genero
-            printf("-Escolha o genero-\n\n");
-            printf("A - Masculino\nB - Feminino\nC - Unisex\n\n");
-            printf("Opcao: ");
-            scanf("%c", &opcao);
-            scanf("%c", &aux);
-
-            switch (opcao)
-            {
-            case 'a':
-            case 'A':
-                strcpy(genero, "Masculino");
-                break;
-            case 'b':
-            case 'B':
-                strcpy(genero, "Feminino");
-                break;
-            case 'c':
-            case 'C':
-                strcpy(genero, "Unisex");
-                break;
-            default:
-                printf("Opcao Invalida!!!\n");
-                break;
-            }
-
-            //tipo de logo
-
-            printf("-Escolha o tipo de logo-\n\n");
-            printf("A - Bordada - Acrescimo de RS10.00\nB - Colada\nC - Junta\n\n");
-            printf("Opcao: ");
-            scanf("%c", &opcao);
-            scanf("%c", &aux);
-
-            switch (opcao)
-            {
-            case 'a':
-            case 'A':
-                strcpy(tipo_de_logo, "Bordada");
-                valor += 10.00;
-                break;
-            case 'b':
-            case 'B':
-                strcpy(tipo_de_logo, "Colada");
-                break;
-            case 'c':
-            case 'C':
-                strcpy(tipo_de_logo, "Junta");
-                break;
-            default:
-                printf("Opcao Invalida!!!\n");
-                break;
-            }
-
-            //tecido
-
-            printf("-Escolha a composicao do tecido-\n\n");
-            printf("A - Poliester\nB - Malha fria\nC - Algodao\nD - Dry Fit - Acrescimo de RS10.00\n\n");
-            printf("Opcao: ");
-            scanf("%c", &opcao);
-            scanf("%c", &aux);
-
-            switch (opcao)
-            {
-            case 'a':
-            case 'A':
-                strcpy(composicao_do_tecido, "Poliester");
-                break;
-            case 'b':
-            case 'B':
-                strcpy(composicao_do_tecido, "Malha fria");
-                break;
-            case 'c':
-            case 'C':
-                strcpy(composicao_do_tecido, "Algodao");
-                break;
-            case 'd':
-            case 'D':
-                strcpy(composicao_do_tecido, "Dry Fit");
-                valor += 10.00;
-                break;
-            default:
-                printf("Opcao Invalida!!!\n");
-                break;
-            }
-
-            //tamanho
-            printf("Digite o tamanho: ");
-            scanf("%d", &tamanho);
-
-            while (tamanho < 0)
-            {
-                printf("Valor invalido, digite novamente: ");
-                scanf("%d", &tamanho);
-            }
-
-            //Numero da peça
-            printf("Digite o numero da peca: ");
-            scanf("%d", &numero_da_peca);
-
-            while (numero_da_peca < 0)
-            {
-                printf("Valor invalido, digite novamente: ");
-                scanf("%d", &numero_da_peca);
-            }
-
-            //Garantia do fornecedor
-            garantia_do_fornecedor = 2;
-
+            Produto p;
+            p = pesquisaProduto();
+        
             system("cls");
 
-            printf("\n-----Detalhes da compra-----");
-            printf("\n");
-            printf("\n");
-            printf("Tipo de peca: ");
-            puts(tipo_de_peca);
-            printf("\n");
-            printf("Marca: ");
-            puts(marca);
-            printf("\n");
-            printf("Cor: ");
-            puts(cor);
-            printf("\n");
-            printf("Genero: ");
-            puts(genero);
-            printf("\n");
-            printf("Tamanho: %d", tamanho);
-            printf("\n");
-            printf("\n");
-            printf("Valor: %.2f", valor);
-            printf("\n");
-            printf("\n");
-            printf("Numero da peca: %d", numero_da_peca);
-            printf("\n");
-            printf("\n");
-            printf("Tipo de logo: ");
-            puts(tipo_de_logo);
-            printf("\n");
-            printf("Composicao do tecido: ");
-            puts(composicao_do_tecido);
-            printf("\n");
-            printf("Garantia do fornecedor: %d meses", garantia_do_fornecedor);
-            printf("\n");
-            printf("\n");
+            imprimeCompra(p);
 
             printf("Obrigada pela compra, o que deseja fazer agora?\n\n");
             printf("1 - Finalizar compra\n2 - Cancelar compra\n");
